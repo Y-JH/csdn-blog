902426fc6f405b2014ba0a725194d21c10153651
yuannunhua <yuannunhua@gmail.com>
2018-07-06 09:36:22 +0800

    add: add a react-native-easy-toast@1.1.0

--- App.js
+++ App.js
@@ -4,54 +4,65 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableWithoutFeedback
 } from 'react-native';
+import Toast, {DURATION} from 'react-native-easy-toast';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+
+    _toast(){
+        this.refs.toast.show("hello world.", DURATION.LENGTH_SHORT);
+    }
+
+    render() {
+        return (<TouchableWithoutFeedback style={styles.container} onPress = {this._toast.bind(this)}>
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Welcome to React Native!
+                    </Text>
+                    <Text style={styles.instructions}>
+                        To get started, edit App.js
+                    </Text>
+                    <Text style={styles.instructions}>
+                        {instructions}
+                    </Text>
+
+                    <Toast ref = 'toast'/>
+                </View>
+            </TouchableWithoutFeedback>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
